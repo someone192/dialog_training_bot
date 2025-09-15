504c930f157d0aa9a086b6a5645ace5537450398
Valeriy <yavalera56@gmail.com>
2025-09-15 04:15:56 +0000

    stg

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.fsm.state import State, StatesGroup
