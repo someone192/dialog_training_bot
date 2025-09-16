55157b7465596258496f9a2a929ec4ff7162a3a3
Valeriy <yavalera56@gmail.com>
2025-09-16 03:27:50 +0000

    sth

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import CallbackQuery, Message, User
 from aiogram_dialog import Dialog, DialogManager, StartMode, Window, setup_dialogs
-from aiogram_dialog.widgets.text import Const #Here we will import widgets we need
+from aiogram_dialog.widgets.text import Format #Here we will import widgets we need
 from environs import Env
+from pprint import pprint
 
 env = Env()
 env.read_env()
@@ -22,12 +23,16 @@ class StartSG(StatesGroup):
 async def some_handler(callback: CallbackQuery, dialog_manager: DialogManager): #There will be handlers here
     pass
 
-async def some_getter(**kwargs): #We will create getters here
-    pass
+async def username_getter(**kwargs): #We will create getters here dialog_manager: DialogManager, event_from_user: User, 
+    pprint(kwargs)
+    return {'username': 'Valeriy'}
 
 #It is a start dialog
 start_dialog = Dialog(
     Window(
+        Format('Hello, {username}!'),
+        getter=username_getter,
+        state=StartSG.start
         #we will add widgets and getters here
     ),
 )
@@ -36,6 +41,7 @@ start_dialog = Dialog(
 async def command_start_process(message: Message, dialog_manager: DialogManager):
     await dialog_manager.start(state=StartSG.start, mode=StartMode.RESET_STACK)
 
-dp.include_router(start_dialog)
-setup_dialogs(dp)
-dp.run_polling(bot)
+if __name__ == '__main__':
+    dp.include_router(start_dialog)
+    setup_dialogs(dp)
+    dp.run_polling(bot)
